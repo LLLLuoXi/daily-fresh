b075c1d7e4446414c22ddd56d3b3c9343b4c066f
luoxi <2652445066@qq.com>
2022-03-16 21:48:11 +0800

    ➕feat(*):完成购买数量的本地存储

--- src/components/TabBar/index.vue
+++ src/components/TabBar/index.vue
@@ -1,6 +1,6 @@
 <!--
  * @Author: luoxi
- * @LastEditTime: 2022-03-15 22:50:14
+ * @LastEditTime: 2022-03-15 23:45:59
  * @LastEditors: your name
  * @Description: 
 -->
@@ -9,7 +9,11 @@
     <van-tabbar fixed route>
       <van-tabbar-item icon="bag-o" to="/">首页</van-tabbar-item>
       <van-tabbar-item icon="apps-o" to="/home/classify">分类</van-tabbar-item>
-      <van-tabbar-item icon="cart-o" to="/home/shopping" :badge="badge"
+      <van-tabbar-item
+        icon="cart-o"
+        to="/home/shopping"
+        :badge="badge"
+        id="shop-car"
         >购物车</van-tabbar-item
       >
       <van-tabbar-item icon="manager-o" to="/my">我的</van-tabbar-item>
---
+++ src/tools/animate/index.vue
@@ -0,0 +1,43 @@
+<!--
+ * @Author: luoxi
+ * @LastEditTime: 2022-03-16 00:11:26
+ * @LastEditors: your name
+ * @Description: 
+-->
+<template>
+  <div
+    v-if="exist"
+    class="item"
+    :style="{
+      width: `${width}px`,
+      height: `${height}px`,
+      opcity: opcity,
+      transform: `translate(${moveX}px, ${moveY}px) scale(${sx}, ${sy})`,
+    }"
+  >
+    <img :src="src" />
+  </div>
+</template>
+
+<script>
+export default {
+  mounted() {
+    console.log(this.moveX, this.opcity);
+  },
+};
+</script>
+
+<style lang="less" scope>
+.item {
+  position: fixed;
+  z-index: 100;
+  top: 0;
+  left: 0;
+  transition: all 0.7s;
+  transform-origin: 0% 0%;
+}
+img {
+  width: 100%;
+  border-radius: 50%;
+}
+</style>
